c44bd5f181cb5c332d99de94047352aa60261e15
javedhamdan107 <javedhamdan@cet.ac.in>
2023-08-02 09:55:16 +0530

    calculator added

---
+++ app.ts
@@ -0,0 +1,9 @@
+import Calculator from "./calculator";
+
+const calculator= new Calculator();
+console.log(calculator.add(3,2));
+console.log(calculator.subtract(10,5));
+console.log(calculator.multiply(5,7));
+console.log(calculator.divide(20,4));
+console.log(calculator.modulus(10,3));
+console.log(calculator.power(2,3));
